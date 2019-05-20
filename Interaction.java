c5ba4368cb0778999df498e898d9a334f6745382
Adele <adele.prouvost@gmail.com>
2019-05-20 17:24:02 +0200

    ajout de la fonction de scroll

---
+++ Interaction.java
@@ -0,0 +1,35 @@
+package Domus;
+
+import Camera_P2I.DetectionMain;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Interaction {
+
+    private ArrayList<Point> accelerationMeasures;
+    private double speed;
+    private double slowingCoef;
+
+    public Interaction(){
+        accelerationMeasures = new ArrayList<Point>();
+    }
+
+    public double getScrollSpeed (boolean scroll, DetectionMain hand){
+        while (scroll== true){
+            Point handCoordinate= new Point( ( hand.getHandCoordinates()[0]), hand.getHandCoordinates()[1]);
+            accelerationMeasures.add(handCoordinate);
+        }
+        speed= (accelerationMeasures.get(accelerationMeasures.size()).y - accelerationMeasures.get(0).y)/ accelerationMeasures.size();
+
+        return speed;
+    }
+
+    public void decreasingScrollSpeed () throws AWTException{
+        Robot r = new Robot();
+        int avgSpeed= (int)(speed*slowingCoef);
+        r.mouseWheel(avgSpeed);
+        speed= speed*slowingCoef;
+    }
+
+}
